3af9ed4d792af5cc4133ef298ba9523f9aa96b76
Zachary Gingras <2039379@csfoy.ca>
2022-04-06 16:52:43 -0400

    Completion du dockerfile et du runDocker.sh

---
+++ runDocker.sh
@@ -0,0 +1,7 @@
+docker stop tp4gbzg_cnt
+docker container rm tp4gbzg_cnt
+docker volume rm tp4zg_vol
+docker image rm tp4gbzg_img
+docker build -t tp4gbzg_img -f ./project/docker/Dockerfile .
+docker volume create --name tp4gbzg_vol --opt device=$PWD --opt o=bind --opt type=none
+docker run -d -p 5555:5555 --mount source=tp4gbzg_vol,target=/mnt/app/ --name tp4gbzg_cnt tp4gbzg_img
