0bed7bdd72e91dbf5141de02ed23f5340469367e
Zachary Gingras <2039379@csfoy.ca>
2022-04-08 08:51:59 -0400

    Modification du requirement et du runDocker.sh afin qu'ils soient comme vus en classe.

--- runDocker.sh
+++ runDocker.sh
@@ -1,6 +1,6 @@
 docker stop tp4gbzg_cnt
 docker container rm tp4gbzg_cnt
-docker volume rm tp4zg_vol
+docker volume rm tp4gbzg_vol
 docker image rm tp4gbzg_img
 docker build -t tp4gbzg_img -f ./project/docker/Dockerfile .
 docker volume create --name tp4gbzg_vol --opt device=$PWD --opt o=bind --opt type=none
